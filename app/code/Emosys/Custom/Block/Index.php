93d35766261069d9f7a7d85bd4c36ea058a9f1df
giangsoda <giangsoda@gmail.com>
2016-02-18 10:03:21 +0700

    create theme + module basic

---
+++ app/code/Emosys/Custom/Block/Index.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright © 2015 Ihor Vansach (ihor@magefan.com). All rights reserved.
+ * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+
+namespace Emosys\Custom\Block;
+
+/**
+ * Blog index block
+ */
+class Index extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * Preparing global layout
+     *
+     * @return $this
+     */
+    protected function _prepareLayout()
+    {
+        $this->_addBreadcrumbs();
+        return parent::_prepareLayout();
+    }
+
+    /**
+     * Prepare breadcrumbs
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @return void
+     */
+    protected function _addBreadcrumbs()
+    {
+        if ($breadcrumbsBlock = $this->getLayout()->getBlock('breadcrumbs')) {
+            $breadcrumbsBlock->addCrumb(
+                'home',
+                [
+                    'label' => __('Home'),
+                    'title' => __('Go to Home Page'),
+                    'link' => $this->_storeManager->getStore()->getBaseUrl()
+                ]
+            );
+            $breadcrumbsBlock->addCrumb(
+                'emosys',
+                [
+                    'label' => __('emosys'),
+                    'title' => __('Emosys')
+                ]
+            );
+        }
+    }
+    
+    public function getDataTest() {
+        return array(1,2,3,4,);
+    }
+}
